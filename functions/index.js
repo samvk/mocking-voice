6ef8fbaaf02b9b679a17eb19172fdf1c3651bbce
Sam Kauffman <samvnkauffman@gmail.com>
2019-01-15 13:05:53 -0500

    revert "add profanity filter" see aecc3e5b

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,5 @@
 const { dialogflow, SimpleResponse } = require('actions-on-google');
 const functions = require('firebase-functions');
-const profanityFilter = require('profanity-filter');
 
 const app = dialogflow({ debug: true });
 
@@ -41,8 +40,8 @@ app.intent(['Default Fallback Intent', 'mock'], (conv, { phrase }) => {
 </speak>`;
 
     conv.close(new SimpleResponse({
-        speech: profanityFilter(speechResponse, (word) => `<say-as interpret-as="expletive">${word}</say-as>`),
-        text: profanityFilter(textResponse, (word) => word.replace(/./g, '*')),
+        speech: speechResponse,
+        text: textResponse,
     }));
 });
 
